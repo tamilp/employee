3d814a20fa362c2efbd74c595b8811913eb4c81a
Purushothaman <tamilselvam.purushothaman@delta.com>
2017-07-11 14:23:39 -0400

    Add code to the source control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    constructor(private http: Http){}
+
+    empID = '';
+    displayEmpfirstname; displayEmplastname; displayEmpage; displayEmpgender; displayEmpemail; displayEmpphone; displayEmpaddress; displayEmpcompany = '';
+
+   /* myFavLang = [
+      {'name': 'html', 'type': 'frontend'},
+      {'name': 'html', 'type': 'frontend'},
+      {'name': 'html', 'type': 'frontend'},
+      {'name': 'html', 'type': 'frontend'},
+    ];*/
+
+    ngOnInit() {
+      this.http.get('http://localhost:3000/employees')
+      .subscribe(
+        (res: Response)=>{
+         const empDetailsDisplay = res.json();
+          console.log(empDetailsDisplay);
+        }
+      )   
+    }
+
+    searchEmp(){
+      this.http.get('http://localhost:3000/employees/' + this.empID)
+      .subscribe(
+        (res: Response)=>{
+          const empDetails = res.json();
+          console.log(empDetails);
+          this.displayEmpfirstname = empDetails.firstname;
+          this.displayEmplastname = empDetails.lastname;
+          this.displayEmpage = empDetails.age;
+          this.displayEmpgender = empDetails.gender;
+          this.displayEmpemail = empDetails.email;
+          this.displayEmpphone = empDetails.phone;
+          this.displayEmpaddress = empDetails.address;
+          this.displayEmpcompany = empDetails.company;
+        }
+      )
+    }
 }
