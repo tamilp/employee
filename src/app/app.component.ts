3c7509670147250951708ee0b1f4f91704a60097
Purushothaman <tamilselvam.purushothaman@delta.com>
2017-07-13 14:57:42 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,30 @@ import { Http, Response } from '@angular/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    constructor(private https: Http){}
+    constructor(private http: Http){}
 
-    empID = '';
-    displayEmpfirstname; displayEmplastname; displayEmpage; displayEmpgender; displayEmpemail; displayEmpphone; displayEmpaddress; displayEmpcompany = '';
+     singleEmpShowHide: boolean = false;
+     empListShowHide: boolean = true;
 
-   /* myFavLang = [
-      {'name': 'html', 'type': 'frontend'},
-      {'name': 'html', 'type': 'frontend'},
-      {'name': 'html', 'type': 'frontend'},
-      {'name': 'html', 'type': 'frontend'},
-    ];*/
+    empDetailsDisplay: '';
+    empID; displayEmppicture; displayEmpfirstname; displayEmplastname; displayEmpage; displayEmpgender; displayEmpemail; displayEmpphone; displayEmpaddress; displayEmpcompany = '';
 
     ngOnInit() {
-      this.https.get('http://localhost:3000/employees')
+      this.http.get('http://localhost:3000/employees')
       .subscribe(
-        (res: Response)=>{
-         const empDetailsDisplay = res.json();
-          console.log(empDetailsDisplay);
-        }
+        empDetailsDisplay => this.empDetailsDisplay = empDetailsDisplay.json(), //Bind to view
+        error => { console.log(error); } // Log errors if any 
       )   
     }
 
     searchEmp(){
-      this.https.get('http://localhost:3000/employees/' + this.empID)
+      
+      this.http.get('http://localhost:3000/employees/' + this.empID)
       .subscribe(
         (res: Response)=>{
           const empDetails = res.json();
           console.log(empDetails);
+           this.displayEmppicture = empDetails.picture;
           this.displayEmpfirstname = empDetails.firstname;
           this.displayEmplastname = empDetails.lastname;
           this.displayEmpage = empDetails.age;
@@ -45,5 +41,16 @@ export class AppComponent {
           this.displayEmpcompany = empDetails.company;
         }
       )
+      
+      if (this.empID != '') {
+        console.log("value");
+        this.singleEmpShowHide = true
+        this.empListShowHide = false
+      }else{
+        console.log("not value")
+        this.singleEmpShowHide = false
+        this.empListShowHide = true
+      }
+      
     }
 }
