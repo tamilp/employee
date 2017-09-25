15a1c80fa19cca113d40ca0a4fd3085edd4ef726
Tom <tamilselvam.purushothaman@delta.com>
2017-09-25 13:04:23 -0400

    New Changes

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+// https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
